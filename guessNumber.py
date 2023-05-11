c8c9610762768cab59f5af4f2cc7bceab3f924bf
hyena <mesutkee@gmail.com>
2023-05-11 17:23:28 +0630

    fix some error using int() need to add int while asking the inputs

--- guessNumber.py
+++ guessNumber.py
@@ -28,32 +28,38 @@ class UserProfile:
 	def winrate(self):
 		winratePer = (self.win*100)/self.plays
 		return winratePer
-	def plays(self):
+	def playE(self):
 		self.plays +=1
-	def win(self):
+	def winE(self):
 		self.win +=1
-	def lost(self):
+	def lostE(self):
 		self.lost+=1
 def play(user,user_profile):
-	user_input = input("enter 1 to play")
-	if int(user_input) == 1:
-		while True:
-			guess=input(f"{user.user_name} guess the number:")
-			answer = random.randint(1,7)
-			if int(guess)==answer:
-				print("huray the answer is correct")
-				user_profile.plays()
-				user_profile.win()
-				print(gameData)
-			else:
-				print("wrong answer")
-				user_profile.plays()
-				user_profile.lost()
-			pOrExit = input("to play again enter>>1, to exit enter>>0:")
-			if pOrExit == 1:
-				continue
-			else:
-				break
+	while True:
+		user_input = input("enter 1 to play or enter 2 to look profile")
+		if int(user_input) == 1:
+			while True:
+				guess=input(f"{user.user_name} guess the number:")
+				answer = random.randint(1,3)
+				if int(guess)==answer:
+					print("huray the answer is correct")
+					user_profile.playE()
+					user_profile.winE()
+				else:
+					print("wrong answer")
+					print(f"answer is {answer}")
+					user_profile.playE()
+					user_profile.lostE()
+				pOrExit = input("to play again enter>>1, to exit enter>>0:")
+				if int(pOrExit) == 1:
+					pass
+				else:
+					break
+		elif int(user_input)==2:
+			print('matches ',user_profile.plays)
+			print('win ',user_profile.win)
+			print('lost ',user_profile.lost)
+			print('winrate ',user_profile.winrate())
 def main():
 	while True:
 		loginOregister = input("login for 1 and register for 2: ")
@@ -66,14 +72,16 @@ def main():
 			userbalance1 = input("user balance:")
 			user = User(username1,userage1,userbalance1,len(userData))
 			user_profile = UserProfile(user,0,0,0)
-			userData.update({username1:{user}})
-			gameData.update({username1:{user_profile}})
+			userData.update({username1:user})
+			gameData.update({username1:user_profile})
 			print("registeration success")
 			print(userData)
 			continue
 		else:
 			userName = input("please enter your userName:")
 			if userName in userData:
+				user1 = userData[str(userName)]
+				print(user1.user_name)
 				play(userData[str(userName)],gameData[str(userName)])
 			else:
 				print("incorrect user name please try again")
