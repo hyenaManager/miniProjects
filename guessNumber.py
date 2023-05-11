dbfcec4c8334b34ca643937860417743ab06cc58
hyena <mesutkee@gmail.com>
2023-05-11 16:58:22 +0630

    setting up the user and userProfile style

--- guessNumber.py
+++ guessNumber.py
@@ -10,12 +10,15 @@ class User:
 		self.user_balance = user_balance
 		self.user_id = user_id
 
+	def __str__(self) -> str:
+		return self.user_name
+
 	def balanceEdit(self,value,modifier):
 		if modifier == "+":
 			self.user_balance +=value
 		else:
 			self.user_balance -= value
-class Activities:
+class UserProfile:
 	def __init__(self,user,plays,win,lost):
 		self.user = user
 		self.plays = plays
@@ -31,19 +34,21 @@ class Activities:
 		self.win +=1
 	def lost(self):
 		self.lost+=1
-def play(user_name):
+def play(user,user_profile):
 	user_input = input("enter 1 to play")
 	if int(user_input) == 1:
 		while True:
-			guess=input("guess the number:")
+			guess=input(f"{user.user_name} guess the number:")
 			answer = random.randint(1,7)
-			if guess==answer:
+			if int(guess)==answer:
 				print("huray the answer is correct")
-				gameData.update()
+				user_profile.plays()
+				user_profile.win()
 				print(gameData)
 			else:
 				print("wrong answer")
-				print(gameData)
+				user_profile.plays()
+				user_profile.lost()
 			pOrExit = input("to play again enter>>1, to exit enter>>0:")
 			if pOrExit == 1:
 				continue
@@ -54,17 +59,22 @@ def main():
 		loginOregister = input("login for 1 and register for 2: ")
 		if int(loginOregister) == 2:
 			username1 = input("user name:")
+			if username1 in userData:
+				print("user name already exist try another name")
+				continue
 			userage1 = input("user age:")
 			userbalance1 = input("user balance:")
-			userR = User(username1,userage1,userbalance1)
-			userR.register()
+			user = User(username1,userage1,userbalance1,len(userData))
+			user_profile = UserProfile(user,0,0,0)
+			userData.update({username1:{user}})
+			gameData.update({username1:{user_profile}})
 			print("registeration success")
 			print(userData)
 			continue
-		else :
+		else:
 			userName = input("please enter your userName:")
 			if userName in userData:
-				play(userName)
+				play(userData[str(userName)],gameData[str(userName)])
 			else:
 				print("incorrect user name please try again")
 
